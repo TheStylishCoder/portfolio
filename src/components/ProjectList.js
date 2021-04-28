df8e641f579f0e0b7bc40583f42c059c15ce6a90
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-28 23:02:40 +0100

    testing layout for portfolio

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,7 +4,11 @@ const ProjectList = ({allProjects, onProjectSelect}) => {
 
     const listItems = allProjects.map((project, index) => {
         return(
-            <li onClick = {()=> {onProjectSelect(project)}} key={index}>{project.projectName}</li>
+            <li onClick = {()=> {onProjectSelect(project)}} key={index}>
+                {project.projectName} 
+                <br />
+                <img src={project.image1} alt="project preview" height = "250" width="auto" /> 
+            </li>
         )
     })
 
