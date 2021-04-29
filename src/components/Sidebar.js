2d32686b00d37c952b940c4eaf87b5b5be2e60c0
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-29 11:13:03 +0100

    added target property to links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,9 +6,9 @@ const Sidebar = () => {
     return(
         <>
         <ul className="sidebar">
-            <li><a href="https://github.com/TheStylishCoder"><i className="fab fa-github fa-2x"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/1annacunningham/"><i className="fab fa-linkedin fa-2x"></i></a></li>
-            <li><a href="mailto:ac.annacunningham@gmail.com"><i className="far fa-envelope fa-2x"></i></a></li>
+            <li><a href="https://github.com/TheStylishCoder" target="_blank" rel="noreferrer"><i className="fab fa-github fa-2x"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/1annacunningham/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a></li>
+            <li><a href="mailto:ac.annacunningham@gmail.com" ><i className="far fa-envelope fa-2x"></i></a></li>
         </ul>
         </>
     )
