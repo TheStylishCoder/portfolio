d10455ffc21d8caee0afcd1237f73c9abda51af6
Anna Cunningham <ac232@hotmail.co.uk>
2021-05-05 19:25:47 +0100

    changed font

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import '../static/css/Homepage.css';
 
 const Homepage = () => {
+
+ 
 
     return(
         <div className="home">
 
             <h1>Anna Cunningham</h1>
             <h3>Full Stack Developer</h3>
+           
+            
         </div>
     )
 }
