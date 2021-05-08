21559edc90e88a46f892cde91e09436bb03fcbfe
Anna Cunningham <ac232@hotmail.co.uk>
2021-05-08 22:33:13 +0100

    changes made for safari compatibility

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,10 +7,9 @@ const Homepage = () => {
 
     return(
         <div className="home">
+            <span><h1>Anna Cunningham</h1></span>
 
-            <h1>Anna Cunningham</h1>
-            <h2>Full Stack Developer</h2>
-           
+            <span><h2>Full Stack Developer</h2></span>
     
         </div>
     )
